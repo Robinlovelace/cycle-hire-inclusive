c783bd36f9bd2ab3af3a47ac5619a57ae7cda5d4
Robin Lovelace <rob00x@gmail.com>
2020-02-21 14:14:03 +0000

    Update with changes

--- R/get_london_cycle_hire_data.R
+++ R/get_london_cycle_hire_data.R
@@ -13,11 +13,12 @@ lchs_download = function(
   message("These files found: ")
   print(paste(list.files(data_dir), sep = "\n"))
   bikedata::dl_bikedata(city = "london", data_dir = data_dir)
+  # bikedata::store_bikedata(bikedb = sqf)
 }
 
 lchs_rename = function(
   data_dir = "data/london",
-  sqf = file.path(data_dir, "london_bike_hire_2020-02-21.sqlite"),
+  sqf = file.path(data_dir, "london_bike_hire_20-02-21.sqlite"),
   files_to_rename = "2020"
 ) {
   message("Changing file namesm removing ., _ and   to make file nemes clean")
@@ -25,16 +26,16 @@ lchs_rename = function(
   if(nchar(rename_pkg_version) < 5) 
     message("Install rename on Mac with brew install rename or Linux with apt install rename or similar")
   message("Using rename version ", rename_pkg_version)
-  msg = paste0("cd .", data_dir, '; mkdir ', files_to_rename, "; mv -v *", files_to_rename, "* ", files_to_rename)
+  msg = paste0("cd ./", data_dir, '; mkdir ', files_to_rename, "; mv -v *", files_to_rename, "* ", files_to_rename)
   message("Running: ", msg)
   system(msg)
-  msg = paste0("cd .", data_dir, '; rename -v "s/[- ]//g" *.csv')
+  msg = paste0("cd ./", data_dir, '; rename -v "s/[- ]//g" *.csv')
   message("Running: ", msg)
   system(msg)
   
-  system("cd -")
+  system("pwd # no need to cd -")
   message("Updating the database")
-  ntrips = bikedata::store_bikedata(bikedb = sqf, data_dir = "data/london")
+  ntrips = bikedata::store_bikedata(bikedb = sqf, data_dir = data_dir)
   message(ntrips, " trips in the updated db")
 }
 
@@ -47,6 +48,30 @@ lchs_read_raw = function(
   message("These tables found: ", dbi_tables)
   trips = tbl(bikes_data, "trips")
   collect(trips)
+}
+
+lchs_filter_select = function(data_raw) {
+  data_raw %>% 
+    select(-city, -user_type, -birth_year, -gender) %>% 
+    distinct(start_time, stop_time, start_station_id, end_station_id) 
+}
+
+lchs_clean = function(data_filtered) {
+  # data_filtered$start_time = lubridate::ymd_hm(data_filtered$start_time) 
+  # data_filtered$stop_time = lubridate::ymd_hm(data_filtered$stop_time) # fast str to POSIXct
+  data_filtered$year_month = lubridate::floor_date(data_filtered$start_time, unit = "month")
+  data_filtered$start_station_id = str_remove(data_filtered$start_station_id, "lo")
+  data_filtered$end_station_id = str_remove(data_filtered$end_station_id, "lo")
+  data_filtered
+}
+
+lchs_check_dates = function(data_filtered_clean_dates) {
+  # # time analysis
+  # trips_per_year = trips_df %>%
+  #   group_by(year_month) %>%
+  #   summarise(
+  #     total = n()
+  #   )
 }
 
 # ntrips : 77694197 (2020-02-20)
@@ -93,6 +118,210 @@ lchs_read_raw = function(
 
 # Notes and test code -----------------------------------------------------
 
+
+
+# # Less than 1 minute to load 68 million rows:
+# system.time({
+#   trips_df = vroom::vroom("london_bike_hire_from_bikedata-2020-02.csv.gz")
+# })
+# # user  system elapsed 
+# # 53.546  10.961  37.546 
+# trips_df
+  
+# 
+
+
+# very slow to write...
+# system.time({
+#   vroom::vroom_write(trips_df, "london_bike_hire_from_bikedata-2020-02-clean-1.csv.gz")
+# })
+# user  system elapsed 
+# 503.784   3.107 435.412 
+
+
+# Cleaning stage II find missing/duplicate data ----------------------
+
+# trips_df = vroom::vroom("london_bike_hire_from_bikedata-2020-02-clean-1.csv.gz")
+# Explore trips/month to find missing/duplicate data ----------------------
+# trips_df_1pct = trips_df %>% sample_frac(size = 0.01)
+# stations = readRDS("stations-clean.Rds")
+# stations <- read_csv("./data/bikelocations_london.csv")
+# trips_with_origin_station_ids = trips_df$start_station_id %in% stations$check_id # 
+# sum(trips_with_origin_station_ids) / nrow(trips_df) # no matching ids!
+# str_remove(trips_df$start_station_id[1:9], "lo")
+# trips_df$start_station_id = str_remove(trips_df$start_station_id, "lo")
+# trips_df$end_station_id = str_remove(trips_df$end_station_id, "lo")
+
+# EDIT : let's write current dataset out before editing on cleaning.
+# ntrips : 77694197 -- with 2015 data properly uploaded
+# fst::write_fst(trips_df, "./data/trips-2020-02.fst")
+# EDIT : Struggling to upload.
+# piggyback::pb_upload(file="./data/trips-2020-02.fst", name="trips-2020-02.fst", repo="Robinlovelace/cycle-hire-inclusive")
+
+# trips_with_origin_station_ids = trips_df$start_station_id %in% stations$ucl_id
+# trips_with_destination_station_ids = trips_df$end_station_id %in% stations$operator_intid
+# sum(trips_with_origin_station_ids) / nrow(trips_df) # 99.2% have origin id
+# sum(trips_with_destination_station_ids) / nrow(trips_df) # 98.1% have destination id
+# trips_with_ids = trips_with_origin_station_ids & trips_with_destination_station_ids
+# # trips_df$with_ids = trips_with_ids
+# sum(trips_with_ids) / nrow(trips_df) # 97% have id
+# yrs_without_ids = lubridate::year(trips_df$start_time[!trips_with_ids])
+# table(yrs_without_ids)
+# # original:
+# # 1900   1901   2012   2013   2014   2015   2016   2017   2018   2019 
+# #  753    420 125218 150010    130    196 434829 731076 905495 447993 
+# # with latest bikedata data:
+# # 1900   1901   2012   2013   2014   2015   2016   2017   2018   2019 
+# # 753    420 342285 285146 445611 288976 228780 171672 206601 268226
+
+
+# trips_df = trips_df %>% filter(trips_with_ids)
+# head(trips_df$start_station_id)
+# trips_by_origin_station = trips_df %>% 
+#   group_by(id = start_station_id) %>% 
+#   summarise(total_n_trips_start = n())
+# 
+# sum(is.na(trips_df$year)) / nrow(trips_df) # 0.2% with no year
+# trips_df = trips_df %>% filter(!is.na(year))
+# has_sane_year = trips_df$year > 2009
+# sum(has_sane_year) / nrow(trips_df)
+# summary(trips_df$year)
+# trips_df
+# trips_df = trips_df %>% select(-bike_id)
+# pryr::object_size(trips_df)
+# # 5.32 GB
+
+# system.time(fst::write_fst(trips_df, "trips_df.fst"))
+# # user  system elapsed  # 100 times faster!
+# # 6.993   1.046   4.464
+# system.time(fst::write_fst(trips_df, "trips_df.fst", 80))
+# # user  system elapsed  
+# # 37.238   1.218  15.196 
+# file.size("trips_df.fst") / 1e9 # 1.3 GB
+# piggyback::pb_upload("trips_df.fst", repo = "itsleeds/tds")
+# 
+# 
+# # identify duplicate/missing months ---------------------------------------
+# 
+# trips_df = fst::read.fst("trips_df.fst")
+# 
+# # time analysis
+# trips_per_year = trips_df %>%
+#   group_by(year_month) %>%
+#   summarise(
+#     total = n()
+#   )
+# g = ggplot(trips_per_year) +
+#   geom_line(aes(year_month, total), col = "grey") 
+# g
+# 
+# trips_df$date = as.Date(trips_df$start_time)
+# trips_per_day = trips_df %>% 
+#   group_by(date) %>% 
+#   summarise(
+#     total_csvs = n()
+#   ) 
+# 
+# trips_per_day_xls = readRDS("daily_hires.Rds")
+# trips_per_day_xls$date = trips_per_day_xls$Day
+# trips_daily = left_join(trips_per_day_xls, trips_per_day)
+# 
+# ggplot(trips_daily, aes(Day, `Number of hires`)) +
+#   geom_point(alpha = 0.1) +
+#   geom_line(aes(Day, Monthly), lwd = 1) +
+#   geom_line(aes(Day, Yearly), colour = "blue", lwd = 1) +
+#   # csv data
+#   geom_point(aes(y = total_csvs), colour = "red", alpha = 0.1) 
+# # finding: there are duplicate trips in the csv files
+# 
+# trips_df_duplicated = duplicated(trips_df %>% select(id))
+# summary(trips_df_duplicated) # but there are no duplicated duplicate ids!
+# trips_df_duplicated = distinct(trips_df %>% select(id))
+# nrow(trips_df_duplicated) / nrow(trips_df) # verified in tidyverse...
+# head(trips_df$start_time) # to nearest minute...
+# trips_df_duplicated = distinct(trips_df %>% select(start_time, stop_time, start_station_id))
+# nrow(trips_df_duplicated) / nrow(trips_df) # 82%
+# trips_df_distinct = trips_df %>% 
+#   distinct(start_time, stop_time, start_station_id, end_station_id)
+# nrow(trips_df_distinct) / nrow(trips_df) # 82.7 %
+# trips_df = trips_df_distinct
+# 
+# trips_per_day = trips_df %>% 
+#   mutate(date = as.Date(start_time)) %>% 
+#   group_by(date) %>% 
+#   summarise(
+#     total_csvs = n()
+#   ) 
+# 
+# trips_daily = left_join(trips_per_day_xls, trips_per_day)
+# 
+# ggplot(trips_daily, aes(Day, `Number of hires`)) +
+#   geom_point(alpha = 0.1) +
+#   geom_line(aes(Day, Monthly), lwd = 1) +
+#   geom_line(aes(Day, Yearly), colour = "blue", lwd = 1) +
+#   # csv data
+#   geom_point(aes(y = total_csvs), colour = "red", alpha = 0.1) +
+#   xlab("Year") +
+#   ylim(c(0, 50000)) +
+#   xlim(as.POSIXlt(c("2010-01-01", "2019-10-01"))) +
+#   ylab("Number of cycle hire events per day") +
+#   # scale_x_continuous(breaks = 2010:2020)
+#   # scale_x_date(breaks = lubridate::ymd(paste0(2010:2020, "-01-01")))
+#   scale_x_date(date_breaks = "1 year", date_labels = "%Y")
+# 
+# cor(trips_daily$`Number of hires`, trips_daily$total_csvs, use = "complete.obs") ^ 2 # 97.5
+# 
+# fst::write_fst(trips_df, "trips_df.fst")
+
+# with bikedata internal functions ----------------------------------------
+
+# aws_url <- "https://s3-eu-west-1.amazonaws.com/cycling.data.tfl.gov.uk/"
+# doc <- httr::content (httr::GET (aws_url), encoding  =  'UTF-8')
+# nodes <- xml2::xml_children(doc)
+# 
+# flist_zip <- getflist (nodes, type = 'zip')
+# flist_zip <- flist_zip [which (grepl ('usage', flist_zip))]
+# flist_csv <- getflist (nodes, type = 'csv')
+# flist_xlsx <- getflist (nodes, type = 'xlsx')
+
+# with rvest --------------------------------------------------------------
+
+# library(rvest)
+# html_data = xml2::read_html("https://cycling.data.tfl.gov.uk/")
+# lnd_urls = html_data %>% 
+#   rvest::html_nodes(css = "#tbody-content a") %>% 
+#   html_text()
+
+
+# old cleaning code -------------------------------------------------------
+# 
+# # clean ids
+# trips_df$id[1:9]
+# sample_ids = sample(x = trips_df$id, 20000) # look OK
+# sample_ids[1:9]
+# summary(str_detect(string = sample_ids, "lo"))
+
+
+# dtplyr test -------------------------------------------------------------
+
+# library(dtplyr)
+# trips_dt = dtplyr::lazy_dt(trips_df_1pct)
+# system.time({
+#   trips_agg = trips_dt %>% 
+#     group_by(year_month) %>% 
+#     summarise(n = n())
+#   trips_agg_df = as.data.frame(trips_agg)
+# })
+# user  system elapsed 
+# 0.167   0.000   0.060 
+
+# system.time({
+#   trips_agg = trips_df_1pct %>% 
+#     group_by(year_month) %>% 
+#     summarise(n = n())
+# })
+# user  system elapsed 
+# 0.054   0.001   0.056
 
 # for   system('cd ./data/london; rename -v "s/[- ]//g" *.csv; cd -')
 # I got: 
@@ -359,209 +588,5 @@ lchs_read_raw = function(
 # 9b Journey Data Extract 01Sep14-13Sep14.csv renamed as 9bJourneyDataExtract01Sep1413Sep14.csv
 # 9b-Journey-Data-Extract-06Sep15-19Sep15.csv renamed as 9bJourneyDataExtract06Sep1519Sep15.csv
 # /mnt/57982e2a-2874-4246-a6fe-115c199bc6bd/atfutures/repos/cycle-hire-inclusive
-
-# # Less than 1 minute to load 68 million rows:
-# system.time({
-#   trips_df = vroom::vroom("london_bike_hire_from_bikedata-2020-02.csv.gz")
-# })
-# # user  system elapsed 
-# # 53.546  10.961  37.546 
-# trips_df
-  
-# 
-
-
-# very slow to write...
-# system.time({
-#   vroom::vroom_write(trips_df, "london_bike_hire_from_bikedata-2020-02-clean-1.csv.gz")
-# })
-# user  system elapsed 
-# 503.784   3.107 435.412 
-
-
-# Cleaning stage II find missing/duplicate data ----------------------
-
-# trips_df = vroom::vroom("london_bike_hire_from_bikedata-2020-02-clean-1.csv.gz")
-# Explore trips/month to find missing/duplicate data ----------------------
-# trips_df_1pct = trips_df %>% sample_frac(size = 0.01)
-# stations = readRDS("stations-clean.Rds")
-# stations <- read_csv("./data/bikelocations_london.csv")
-# trips_with_origin_station_ids = trips_df$start_station_id %in% stations$check_id # 
-# sum(trips_with_origin_station_ids) / nrow(trips_df) # no matching ids!
-# str_remove(trips_df$start_station_id[1:9], "lo")
-# trips_df$start_station_id = str_remove(trips_df$start_station_id, "lo")
-# trips_df$end_station_id = str_remove(trips_df$end_station_id, "lo")
-
-# EDIT : let's write current dataset out before editing on cleaning.
-# ntrips : 77694197 -- with 2015 data properly uploaded
-# fst::write_fst(trips_df, "./data/trips-2020-02.fst")
-# EDIT : Struggling to upload.
-# piggyback::pb_upload(file="./data/trips-2020-02.fst", name="trips-2020-02.fst", repo="Robinlovelace/cycle-hire-inclusive")
-
-# trips_with_origin_station_ids = trips_df$start_station_id %in% stations$ucl_id
-# trips_with_destination_station_ids = trips_df$end_station_id %in% stations$operator_intid
-# sum(trips_with_origin_station_ids) / nrow(trips_df) # 99.2% have origin id
-# sum(trips_with_destination_station_ids) / nrow(trips_df) # 98.1% have destination id
-# trips_with_ids = trips_with_origin_station_ids & trips_with_destination_station_ids
-# # trips_df$with_ids = trips_with_ids
-# sum(trips_with_ids) / nrow(trips_df) # 97% have id
-# yrs_without_ids = lubridate::year(trips_df$start_time[!trips_with_ids])
-# table(yrs_without_ids)
-# # original:
-# # 1900   1901   2012   2013   2014   2015   2016   2017   2018   2019 
-# #  753    420 125218 150010    130    196 434829 731076 905495 447993 
-# # with latest bikedata data:
-# # 1900   1901   2012   2013   2014   2015   2016   2017   2018   2019 
-# # 753    420 342285 285146 445611 288976 228780 171672 206601 268226
-
-
-# trips_df = trips_df %>% filter(trips_with_ids)
-# head(trips_df$start_station_id)
-# trips_by_origin_station = trips_df %>% 
-#   group_by(id = start_station_id) %>% 
-#   summarise(total_n_trips_start = n())
-# 
-# sum(is.na(trips_df$year)) / nrow(trips_df) # 0.2% with no year
-# trips_df = trips_df %>% filter(!is.na(year))
-# has_sane_year = trips_df$year > 2009
-# sum(has_sane_year) / nrow(trips_df)
-# summary(trips_df$year)
-# trips_df
-# trips_df = trips_df %>% select(-bike_id)
-# pryr::object_size(trips_df)
-# # 5.32 GB
-
-# system.time(fst::write_fst(trips_df, "trips_df.fst"))
-# # user  system elapsed  # 100 times faster!
-# # 6.993   1.046   4.464
-# system.time(fst::write_fst(trips_df, "trips_df.fst", 80))
-# # user  system elapsed  
-# # 37.238   1.218  15.196 
-# file.size("trips_df.fst") / 1e9 # 1.3 GB
-# piggyback::pb_upload("trips_df.fst", repo = "itsleeds/tds")
-# 
-# 
-# # identify duplicate/missing months ---------------------------------------
-# 
-# trips_df = fst::read.fst("trips_df.fst")
-# 
-# # time analysis
-# trips_per_year = trips_df %>% 
-#   group_by(year_month) %>% 
-#   summarise(
-#     total = n()
-#   ) 
-# g = ggplot(trips_per_year) +
-#   geom_line(aes(year_month, total), col = "grey") 
-# g
-# 
-# trips_df$date = as.Date(trips_df$start_time)
-# trips_per_day = trips_df %>% 
-#   group_by(date) %>% 
-#   summarise(
-#     total_csvs = n()
-#   ) 
-# 
-# trips_per_day_xls = readRDS("daily_hires.Rds")
-# trips_per_day_xls$date = trips_per_day_xls$Day
-# trips_daily = left_join(trips_per_day_xls, trips_per_day)
-# 
-# ggplot(trips_daily, aes(Day, `Number of hires`)) +
-#   geom_point(alpha = 0.1) +
-#   geom_line(aes(Day, Monthly), lwd = 1) +
-#   geom_line(aes(Day, Yearly), colour = "blue", lwd = 1) +
-#   # csv data
-#   geom_point(aes(y = total_csvs), colour = "red", alpha = 0.1) 
-# # finding: there are duplicate trips in the csv files
-# 
-# trips_df_duplicated = duplicated(trips_df %>% select(id))
-# summary(trips_df_duplicated) # but there are no duplicated duplicate ids!
-# trips_df_duplicated = distinct(trips_df %>% select(id))
-# nrow(trips_df_duplicated) / nrow(trips_df) # verified in tidyverse...
-# head(trips_df$start_time) # to nearest minute...
-# trips_df_duplicated = distinct(trips_df %>% select(start_time, stop_time, start_station_id))
-# nrow(trips_df_duplicated) / nrow(trips_df) # 82%
-# trips_df_distinct = trips_df %>% 
-#   distinct(start_time, stop_time, start_station_id, end_station_id)
-# nrow(trips_df_distinct) / nrow(trips_df) # 82.7 %
-# trips_df = trips_df_distinct
-# 
-# trips_per_day = trips_df %>% 
-#   mutate(date = as.Date(start_time)) %>% 
-#   group_by(date) %>% 
-#   summarise(
-#     total_csvs = n()
-#   ) 
-# 
-# trips_daily = left_join(trips_per_day_xls, trips_per_day)
-# 
-# ggplot(trips_daily, aes(Day, `Number of hires`)) +
-#   geom_point(alpha = 0.1) +
-#   geom_line(aes(Day, Monthly), lwd = 1) +
-#   geom_line(aes(Day, Yearly), colour = "blue", lwd = 1) +
-#   # csv data
-#   geom_point(aes(y = total_csvs), colour = "red", alpha = 0.1) +
-#   xlab("Year") +
-#   ylim(c(0, 50000)) +
-#   xlim(as.POSIXlt(c("2010-01-01", "2019-10-01"))) +
-#   ylab("Number of cycle hire events per day") +
-#   # scale_x_continuous(breaks = 2010:2020)
-#   # scale_x_date(breaks = lubridate::ymd(paste0(2010:2020, "-01-01")))
-#   scale_x_date(date_breaks = "1 year", date_labels = "%Y")
-# 
-# cor(trips_daily$`Number of hires`, trips_daily$total_csvs, use = "complete.obs") ^ 2 # 97.5
-# 
-# fst::write_fst(trips_df, "trips_df.fst")
-
-# with bikedata internal functions ----------------------------------------
-
-# aws_url <- "https://s3-eu-west-1.amazonaws.com/cycling.data.tfl.gov.uk/"
-# doc <- httr::content (httr::GET (aws_url), encoding  =  'UTF-8')
-# nodes <- xml2::xml_children(doc)
-# 
-# flist_zip <- getflist (nodes, type = 'zip')
-# flist_zip <- flist_zip [which (grepl ('usage', flist_zip))]
-# flist_csv <- getflist (nodes, type = 'csv')
-# flist_xlsx <- getflist (nodes, type = 'xlsx')
-
-# with rvest --------------------------------------------------------------
-
-# library(rvest)
-# html_data = xml2::read_html("https://cycling.data.tfl.gov.uk/")
-# lnd_urls = html_data %>% 
-#   rvest::html_nodes(css = "#tbody-content a") %>% 
-#   html_text()
-
-
-# old cleaning code -------------------------------------------------------
-# 
-# # clean ids
-# trips_df$id[1:9]
-# sample_ids = sample(x = trips_df$id, 20000) # look OK
-# sample_ids[1:9]
-# summary(str_detect(string = sample_ids, "lo"))
-
-
-# dtplyr test -------------------------------------------------------------
-
-# library(dtplyr)
-# trips_dt = dtplyr::lazy_dt(trips_df_1pct)
-# system.time({
-#   trips_agg = trips_dt %>% 
-#     group_by(year_month) %>% 
-#     summarise(n = n())
-#   trips_agg_df = as.data.frame(trips_agg)
-# })
-# user  system elapsed 
-# 0.167   0.000   0.060 
-
-# system.time({
-#   trips_agg = trips_df_1pct %>% 
-#     group_by(year_month) %>% 
-#     summarise(n = n())
-# })
-# user  system elapsed 
-# 0.054   0.001   0.056
-
 
 
--- make.R
+++ make.R
@@ -6,6 +6,7 @@
 
 pkgs = c(
   "drake",
+  "fasttime",
   "leaflet",
   "lubridate",
   "patchwork",
@@ -29,9 +30,17 @@ data_dir = "data/london-new"
 sqf = file.path(data_dir, "london_bike_hire_2020-02-21.sqlite")
 
 plan = drake_plan(
-  download_data = lchs_download(data_dir, sqf),
-  rename_data = lchs_rename(data_dir, sqf, files_to_rename = "2020"),
-  data_raw = lchs_read_raw(data_dir, sqf)
+  # download_data = lchs_download(data_dir, sqf),
+  # rename_data = lchs_rename(data_dir, sqf, files_to_rename = "2020"),
+  # data_raw = lchs_read_raw(data_dir, sqf),
+  # data_raw = target(lchs_read_raw(data_dir, sqf), format = "fst"), # fast version
+  # save_raw_data = fst::write_fst(data_raw, "data_raw.fst", compress = 80), # commented to be faster
+  data_raw = target(fst::read.fst("data_raw.fst"), format = "fst"),
+  data_filtered = target(
+    lchs_filter_select(data_raw)
+    , format = "fst"),
+  data_filtered_clean = lchs_clean(data_filtered)
+  # check_raw_data = 
   # data = raw_data %>%
   #   mutate(Species = forcats::fct_inorder(Species)),
   # hist = create_plot(data),
@@ -43,4 +52,25 @@ plan = drake_plan(
   # )
 )
 
+
+
+# make plan ---------------------------------------------------------------
+
+# vis_drake_graph(plan)
 make(plan)
+# 84950285 rows...
+# make(plan, parallelism = "future", jobs = 2) # worked
+# make(plan, parallelism = "clustermq", jobs = 2) # failed
+
+# # debug(lchs_rename) # to debug
+# undebug(lchs_rename)
+
+
+# upload results ----------------------------------------------------------
+
+# data_raw = readd(data_raw) 
+# data_filtered = readd(data_filtered, verbose = T)
+# data_filtered_clean = readd(data_filtered_clean, verbose = T)
+# fst::write_fst(data_raw, "data_raw.fst", compress = 80)
+# file.size("data_raw.fst") # 2.3 GB = too big!
+# data_raw = fst::read.fst("data_raw.fst")
