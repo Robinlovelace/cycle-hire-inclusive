2d56526e63b2383176180a375c6d1bd11b53e0c6
Robin Lovelace <rob00x@gmail.com>
2020-02-23 19:25:07 +0000

    Update bike hire players code

--- R/get-london-bike-hire-players.R
+++ R/get-london-bike-hire-players.R
@@ -16,4 +16,5 @@ ggplot(bike_companies) +
   geom_col(aes(reorder(Company, -N_bikes), N_bikes, fill = Year)) +
   xlab("")
 ggsave("figures/london-bike-hire-players.png")
-bike_n_bikes = n
+
+bike_companies$N_bikes[1] / sum(bike_companies$N_bikes)
